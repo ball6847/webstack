43743cfd1067ed9c335e8a3afc233277fcd09ed7
Porawit Poboonma <porawit.p@bizidea.co.th>
2013-10-11 14:38:46 +0700

    add supports for apache2.4

--- snipts/addtoapache.sh
+++ snipts/addtoapache.sh
@@ -5,6 +5,42 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-a2dissite default
-ln -s /home/ball6847/webstack/etc/apache2/webstack.conf /etc/apache2/conf.d/
-service apache2 restart
+# disable default site if currently enabled
+if [[ -e "/etc/apache2/sites-enabled/default" ]]; then
+    tobedisabled="$tobedisabled default"
+fi
+
+if [[ -e "/etc/apache2/sites-enabled/000-default.conf" ]]; then
+    tobedisabled="$tobedisabled 000-default"
+fi
+
+if [ "$(echo $tobedisabled | tr -d ' ')" != "" ]; then
+    echo "Disabling sites, $tobedisabled."
+    a2dissite $tobedisabled
+fi
+
+# apache2 now use conf-available instead of conf.d
+configdir="/etc/apache2/conf-available/"
+
+if [[ ! -d "$configdir" ]]; then
+    if [[ ! -d "/etc/apache2/conf.d" ]]; then
+        echo "" 1>&2
+        exit 1
+    fi
+    configdir="/etc/apache2/conf.d/"
+fi
+
+echo "Apache config dir = $configdir"
+
+if [[ $configdir = "/etc/apache2/conf-available/" ]] && [[ ! -f "/etc/apache2/conf-enabled/webstack.conf" ]]; then
+    a2enconf webstack
+fi
+
+# if it's not already in its place
+if [[ ! -f $configdir"webstack.conf" ]]; then
+    echo "Adding webstack config."
+    ln -s /home/ball6847/webstack/etc/apache2/webstack.conf $configdir
+    service apache2 restart
+fi
+
+echo "Webstack should work now."
