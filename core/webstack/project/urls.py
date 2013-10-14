15f4ef238922c5b559cdf8a6b088ecc906504bae
Porawit Poboonma <porawit.p@bizidea.co.th>
2013-10-14 10:36:36 +0700

    add static url handler (for gunicorn)

--- core/webstack/project/urls.py
+++ core/webstack/project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,5 +7,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    # workaround for gunicorn
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
     url(r'^/?', include(admin.site.urls)),
 )
