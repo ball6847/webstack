cdb09c8722d09fb95ca424b081458716f91956f4
Porawit Poboonma <porawit.p@bizidea.co.th>
2013-10-15 16:30:46 +0700

    add command to enable proxy and proxy_http module for apache

--- snipts/addtoapache.sh
+++ snipts/addtoapache.sh
@@ -33,6 +33,7 @@ fi
 echo "Apache config dir = $configdir"
 
 if [[ $configdir = "/etc/apache2/conf-available/" ]] && [[ ! -f "/etc/apache2/conf-enabled/webstack.conf" ]]; then
+    a2enmod proxy proxy_http
     a2enconf webstack.conf
 fi
 
