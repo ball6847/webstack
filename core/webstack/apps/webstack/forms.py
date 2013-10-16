40c95fb009346fddd5e787168940fae16ac9f24b
Porawit Poboonma <ball6847@gmail.com>
2013-10-16 21:41:47 +0700

    fix path hardcode issue, refine comments

--- core/webstack/apps/webstack/forms.py
+++ core/webstack/apps/webstack/forms.py
@@ -1,36 +1,30 @@
-from django  import forms 
-from unipath import Path
-from .utils  import make_clean_filename
+from django      import forms
+from django.conf import settings
+from unipath     import Path
+from .utils      import make_clean_filename
 import re
 
 class ProjectForm(forms.ModelForm):
     
-    """ display path as readonly for existing object """
-    
     def __init__(self, *args, **kwargs):
+        """
+        display path as readonly for existing object
+        """
         super(ProjectForm, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
         if instance and instance.pk:
             self.fields['path'].widget.attrs['readonly'] = True
     
-    """ --------------------------------------------------------- """
-    
-    """ prevent user from updating path on existing object
-        use webstack's default on create new with empty value """
-        
     def clean_path(self):
-        # prevent user from changing docroot on existing object
+        """
+        prevent user from updating path on existing object
+        use webstack's default on create new with empty value
+        """
         instance = getattr(self, 'instance', None)
         if instance and instance.pk:
             return instance.path
-        
         domain = self.cleaned_data['domain']
         path = self.cleaned_data['path']
-        
-        # use webstack's default
         if path == "":
-            path = "/home/ball6847/webstack/www/" + re.sub(r"\.+", "_", domain)
-        
+            path = settings.WEBSTACK_ROOT.child("www", re.sub(r"\.+", "_", domain))
         return path
-    
-    """ --------------------------------------------------------- """
